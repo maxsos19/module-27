18c76d3903b2af34574ebc3c0790329f37b1ef8f
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2024-09-23 01:02:21 +0300

    Add files via upload

---
+++ LifeSpot-master/LifeSpot/Static/JS/about.js
@@ -0,0 +1,22 @@
+﻿const writeReview = reviews => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${reviews['userName']}</b>  ${reviews['date']}</i></p>` +
+        `<p>${reviews['comment']}</p>` +
+        '</div>';
+}
+
+function Reviews() {
+    let reviews = {}
+
+    reviews["userName"] = prompt("Напишите Ваше имя")
+    if (reviews["userName"] == null) {
+        return
+    }
+    reviews["comment"] = prompt("Оставьте свой отзыв")
+    if (reviews["comment"] == null) {
+        return
+    }
+    reviews["date"] = new Date().toLocaleString()
+
+    writeReview(reviews)
+}
